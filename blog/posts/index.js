fa95858c933f8867c94f711edec307a9ca7ed82c
basON <wch.basinski@gmail.com>
2022-12-23 17:20:27 +0100

    Add initial event bus

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const app = express();
@@ -8,10 +9,14 @@ app.use(cors());
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { id, title };
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
   res.status(200).send(posts[id]);
 });
 
